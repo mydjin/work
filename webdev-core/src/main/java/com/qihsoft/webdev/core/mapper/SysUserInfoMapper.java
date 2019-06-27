bc4c2debe1337a0cf1b29f58c57bcf6318292b2e
fdj <2089045617@qq.com>
2019-06-27 15:38:07 +0800

    连接数据库，并在web端进行管理，后台管理完成

--- webdev-core/src/main/java/com/qihsoft/webdev/core/mapper/SysUserInfoMapper.java
+++ webdev-core/src/main/java/com/qihsoft/webdev/core/mapper/SysUserInfoMapper.java
@@ -13,16 +13,16 @@ import java.util.List;
 public interface SysUserInfoMapper extends BaseMapper<SysUserInfo> {
 
     @Select("SELECT a.* " +
-            "FROM v_user_info a " +
-            "JOIN (SELECT id from v_user_info where isDel = 0 ${where} " +
+            "FROM sys_user_info a " +
+            "JOIN (SELECT id from sys_user_info where isDel = 0 ${where} " +
             "LIMIT #{index}, #{size})b ON a.id=b.id order by a.createTime asc ")
     List<JSONObject> getPage(@Param("where") String where,
                              @Param("index") int index,
                              @Param("size") int size);
 
-    @Select("select count(1) total from v_user_info where isDel = 0 ${where} ")
+    @Select("select count(1) total from sys_user_info where isDel = 0 ${where} ")
     JSONObject getPageCount(@Param("where") String where);
 
-    @Select("SELECT a.* FROM v_user_info a where 1=1 and isDel=0 ${where}  order by createTime desc")
+    @Select("SELECT a.* FROM sys_user_info a where 1=1 and isDel=0 ${where}  order by createTime desc")
     List<JSONObject> queryAll(@Param("where") String where);
 }
