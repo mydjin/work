bf81c1bafb37813727d3a9da20bb2349594230dc
fdj <2089045617@qq.com>
2019-06-27 22:30:49 +0800

    1、更改代码生成的数据库连接IP、用户名、密码， 2、更新git的操作 3、更改mapper文件中不适用的部分

--- webdev-core/src/main/java/com/qihsoft/webdev/core/mapper/SysOperationRecordMapper.java
+++ webdev-core/src/main/java/com/qihsoft/webdev/core/mapper/SysOperationRecordMapper.java
@@ -15,20 +15,20 @@ public interface SysOperationRecordMapper extends BaseMapper<SysOperationRecord>
 
 
 
-    @Select("SELECT a.*,FROM_UNIXTIME(createTime) createTimeStr FROM v_operation_record a where 1=1 ${where} order by createTime desc LIMIT #{index}, #{size} ")
+    @Select("SELECT a.*,FROM_UNIXTIME(createTime) createTimeStr FROM sys_operation_record a where 1=1 ${where} order by createTime desc LIMIT #{index}, #{size} ")
     List<JSONObject> getPage(@Param("where") String where,
                              @Param("index") int index,
                              @Param("size") int size);
 
-    @Select("select count(1) total from  v_operation_record where 1=1 ${where} ")
+    @Select("select count(1) total from  sys_operation_record where 1=1 ${where} ")
     JSONObject getPageCount(@Param("where") String where);
 
 
-    @Select("SELECT a.*,FROM_UNIXTIME(createTime) createTimeStr FROM v_operation_record a where 1=1 ${where} order by createTime desc ")
+    @Select("SELECT a.*,FROM_UNIXTIME(createTime) createTimeStr FROM sys_operation_record a where 1=1 ${where} order by createTime desc ")
     List<JSONObject> queryAll(@Param("where") String where);
 
     //微信端获取最近五条登陆日志
-    @Select("SELECT function,ipAddr,status,createTime,createTimeStr  FROM v_operation_record  where 1=1  and number = ${number} and control = '系统授权管理'and server ='1' order by createTime desc LIMIT 3 ")
+    @Select("SELECT function,ipAddr,status,createTime,createTimeStr  FROM sys_operation_record  where 1=1  and number = ${number} and control = '系统授权管理'and server ='1' order by createTime desc LIMIT 3 ")
     List<JSONObject> queryWechatLastLoginLogByNumber(@Param("number") String number);
 
 }
